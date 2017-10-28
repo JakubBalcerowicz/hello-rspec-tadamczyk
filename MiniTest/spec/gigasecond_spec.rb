c84fb1cbf78d257e3b6f6f5a2ba5dd94816ea09a
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-10-28 12:42:24 +0200

    Implement second RSpec in gigasecond

--- MiniTest/spec/gigasecond_spec.rb
+++ MiniTest/spec/gigasecond_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe '../lib/gigasecond.rb' do
       finalDate = Time.utc(2043, 1, 1, 1, 46, 40)
       expect(Gigasecond.from(date)).to eq finalDate
     end
+
+    it "returns 19/2/2009 01:46:40 date" do
+        date = Time.utc(1977, 6, 13, 0, 0, 0)
+        finalDate = Time.utc(2009, 2, 19, 1, 46, 40)
+        expect(Gigasecond.from(date)).to eq finalDate
+    end
   end
 end
