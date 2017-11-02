3ceae67a56363f4941bf734a00c9ba1017f78fef
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-11-02 14:49:18 +0100

    Made string_array_merge_sort_spec.rb, moved string tests from merge_sort_spec.rb and rename merge_sort_spec.rb -> main_merge_sort_spec.rb

--- MergeSort/spec/string_array_merge_sort_spec.rb
+++ MergeSort/spec/string_array_merge_sort_spec.rb
@@ -2,16 +2,8 @@ require_relative '../lib/merge_sort'
 
 RSpec.describe '../lib/merge_sort.rb' do
   describe '#merge_sort' do
-    it 'should not returns raise error' do
-      expect{[1, 2, 3].merge_sort}.not_to raise_error
-    end
-
     it 'should returns correct string array' do
       expect(%w[tomek adam czyk].merge_sort).to be_a_kind_of Array
-    end
-
-    it 'should returns sorted empty array' do
-      expect([].merge_sort).to match([])
     end
 
     it 'should returns sorted array with one string element' do
@@ -35,12 +27,6 @@ RSpec.describe '../lib/merge_sort.rb' do
     it 'should returns sorted string array with duplicate elements' do
       before_sort = %w[tomek tomek tomek adam czyk adam]
       after_sort = %w[adam adam czyk tomek tomek tomek]
-      expect(before_sort.merge_sort).to match_array(after_sort)
-    end
-
-    it 'should returns sorted array includes float, int and string elements' do
-      before_sort = ['Tomek', '15.34', 'adam', '9']
-      after_sort = ['9', '15.34', 'Tomek', 'adam']
       expect(before_sort.merge_sort).to match_array(after_sort)
     end
 
