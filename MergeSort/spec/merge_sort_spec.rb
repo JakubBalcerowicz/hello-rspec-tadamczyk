fea1976e46ce83785577d382a802f67a2dde7139
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-11-02 14:44:15 +0100

    Made float_array_merge_sort_spec.rb and moved float tests from merge_sort_spec.rb

--- MergeSort/spec/merge_sort_spec.rb
+++ MergeSort/spec/merge_sort_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe '../lib/merge_sort.rb' do
       expect{[1, 2, 3].merge_sort}.not_to raise_error
     end
 
-    it 'should returns correct float array' do
-      expect([1.001, 1.1, 0.00001].merge_sort).to be_a_kind_of Array
-    end
-
     it 'should returns correct string array' do
       expect(%w[tomek adam czyk].merge_sort).to be_a_kind_of Array
     end
@@ -18,28 +14,8 @@ RSpec.describe '../lib/merge_sort.rb' do
       expect([].merge_sort).to match([])
     end
 
-    it 'should returns sorted array with one float element' do
-      expect([1.0001].merge_sort).to match_array([1.0001])
-    end
-
     it 'should returns sorted array with one string element' do
       expect(['tomek'].merge_sort).to match_array(['tomek'])
-    end
-
-    it 'should returns sorted float array' do
-      expect([1.001, 1.002, 1.1, 1].merge_sort).to match_array([1, 1.001, 1.002, 1.1])
-    end
-
-    it 'should returns sorted negative float array' do
-      expect([-1.001, -1.002, -1.1, -1].merge_sort).to match_array([-1.1, -1.002, -1.001, -1])
-    end
-
-    it 'should returns correctly sorted float array' do
-      expect([133, 1.531, 0.233, -5.032].merge_sort).to match_array([-5.032, 0.233, 1.531, 133])
-    end
-
-    it 'should returns sorted float array with duplicate elements' do
-      expect([1.1, 1.1, 1, 1.001, 1, 1.002].merge_sort).to match_array([1, 1, 1.001, 1.002, 1.1, 1.1])
     end
 
     it 'should returns sorted string array' do
@@ -66,16 +42,6 @@ RSpec.describe '../lib/merge_sort.rb' do
       before_sort = ['Tomek', '15.34', 'adam', '9']
       after_sort = ['9', '15.34', 'Tomek', 'adam']
       expect(before_sort.merge_sort).to match_array(after_sort)
-    end
-
-    it 'should returns the littlest element in sorted float array' do
-      array = [-1.002, -1.01, 5.2321, 5.232].merge_sort
-      expect(array[0]).to eq (-1.01)
-    end
-
-    it 'should returns the biggest element in sorted float array' do
-      array = [-1.002, -1.01, 5.2321, 5.232].merge_sort
-      expect(array[-1]).to eq (5.2321)
     end
 
     it 'should returns the first one element in sorted string array' do
