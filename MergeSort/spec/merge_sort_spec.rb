edff40f727d492afbe7ea1f70881d3296d9b2ac0
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-11-02 13:29:47 +0100

    Added three more tests that validate array

--- MergeSort/spec/merge_sort_spec.rb
+++ MergeSort/spec/merge_sort_spec.rb
@@ -2,8 +2,20 @@ require_relative '../lib/merge_sort'
 
 RSpec.describe '../lib/merge_sort.rb' do
   describe '#merge_sort' do
-    it 'not returns raise error' do
+    it 'should not returns raise error' do
       expect{[1, 2, 3].merge_sort}.not_to raise_error
+    end
+
+    it 'should returns correct int array' do
+      expect([1, 2, 3].merge_sort).to be_a_kind_of Array
+    end
+
+    it 'should returns correct float array' do
+      expect([1.001, 1.1, 0.00001].merge_sort).to be_a_kind_of Array
+    end
+
+    it 'should returns correct string array' do
+      expect(%w[tomek adam czyk].merge_sort).to be_a_kind_of Array
     end
   end
 end
