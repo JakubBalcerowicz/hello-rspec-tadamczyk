14bc585c7458465fc8fea7aae53254fb4c45cb27
Tomasz Adamczyk <tadamczyk@sigma.ug.edu.pl>
2017-11-02 14:40:58 +0100

    Made int_array_merge_sort_spec.rb file and moved int tests from merge_sort_spec

--- MergeSort/spec/merge_sort_spec.rb
+++ MergeSort/spec/merge_sort_spec.rb
@@ -6,10 +6,6 @@ RSpec.describe '../lib/merge_sort.rb' do
       expect{[1, 2, 3].merge_sort}.not_to raise_error
     end
 
-    it 'should returns correct int array' do
-      expect([1, 2, 3].merge_sort).to be_a_kind_of Array
-    end
-
     it 'should returns correct float array' do
       expect([1.001, 1.1, 0.00001].merge_sort).to be_a_kind_of Array
     end
@@ -22,32 +18,12 @@ RSpec.describe '../lib/merge_sort.rb' do
       expect([].merge_sort).to match([])
     end
 
-    it 'should returns sorted array with one int element' do
-      expect([1].merge_sort).to match_array([1])
-    end
-
     it 'should returns sorted array with one float element' do
       expect([1.0001].merge_sort).to match_array([1.0001])
     end
 
     it 'should returns sorted array with one string element' do
       expect(['tomek'].merge_sort).to match_array(['tomek'])
-    end
-
-    it 'should returns sorted int array' do
-      expect([1321, 1, 23, 422].merge_sort).to match_array([1, 23, 422, 1321])
-    end
-
-    it 'should returns sorted negative int array' do
-      expect([-2, -500, -1, -3].merge_sort).to match_array([-500, -3, -2, -1])
-    end
-
-    it 'should returns correctly sorted int array' do
-      expect([133, 1, 0, -5].merge_sort).to match_array([-5, 0, 1, 133])
-    end
-
-    it 'should returns sorted int array with duplicate elements' do
-      expect([0, 2, 1, 2, 1, 1].merge_sort).to match_array([0, 1, 1, 1, 2, 2])
     end
 
     it 'should returns sorted float array' do
@@ -90,16 +66,6 @@ RSpec.describe '../lib/merge_sort.rb' do
       before_sort = ['Tomek', '15.34', 'adam', '9']
       after_sort = ['9', '15.34', 'Tomek', 'adam']
       expect(before_sort.merge_sort).to match_array(after_sort)
-    end
-
-    it 'should returns the littlest element in sorted int array' do
-      array = [-1, -12, 2, 5].merge_sort
-      expect(array[0]).to eq (-12)
-    end
-
-    it 'should returns the biggest element in sorted int array' do
-      array = [-1, -12, 2, 5].merge_sort
-      expect(array[-1]).to eq (5)
     end
 
     it 'should returns the littlest element in sorted float array' do
